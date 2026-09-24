5554deeaac1179e6f9558c7df569d728d2412e3f
Nathan Frohbieter <n-frohbieter@users.noreply.github.com>
2026-09-23 19:11:58 -0500

    Started Working on Inventory Class

---
+++ prototype/Inventory/inventory_item.py
@@ -0,0 +1,11 @@
+class InventoryItem:
+    def __init__(self, id_number, description):
+        self.id_number = id_number
+        self.description = description
+        self.amount
+        self.location
+        self.price_per_unit
+        self.vendor
+        self.reorder_amount
+        self.minimum_amount
+        self.total_cost
